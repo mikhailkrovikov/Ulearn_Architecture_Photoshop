1a4254b5bfd817852669c768316bcd84d258c522
mkrovikov <mikhailkrovikov@gmail.com>
2026-04-11 21:09:14 +0300

    refactoring: pixel limits

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -2,7 +2,36 @@ namespace MyPhotoshop;
 
 public class Pixel
 {
-    public double R { get; set; }
-    public double G { get; set; }
-    public double B { get; set; }
+    private double _r;
+    private double _g;
+    private double _b;
+    public double R
+    {
+        get => _r;
+        set => _r = Check(value);
+    }
+    public double G
+    {
+        get => _g;
+        set => _g = Check(value);
+    }
+    public double B
+    {
+        get => _b;
+        set => _b = Check(value);
+    }
+
+    private double Check(double value)
+    {
+        if (value <= 1 || value > 0)
+            return value;
+        else throw new ArgumentException();
+    }
+
+    public static double Trim(double value)
+    {
+        if (value < 0) return 0;
+        if (value > 1) return 1;
+        return value;
+    }
 }
