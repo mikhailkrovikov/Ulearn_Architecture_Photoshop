ebe020a65de521b28e92907886ecdd839006dc1a
mkrovikov <mikhailkrovikov@gmail.com>
2026-04-11 21:25:21 +0300

    refactoring: ctor for Photo

--- Data/Photo.cs
+++ Data/Photo.cs
@@ -2,7 +2,18 @@ namespace MyPhotoshop;
 
 public class Photo
 {
-    public int width;
-    public int height;
-    public Pixel[,] data;
+    public readonly int Width;
+    public readonly int Height;
+    public readonly Pixel[,] Data;
+
+    public Photo(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Data = new Pixel[width, height];
+
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                Data[i, j] = new Pixel();
+    }
 }
